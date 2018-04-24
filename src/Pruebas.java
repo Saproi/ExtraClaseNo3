8ac5854bd9994c83850c8af6c05d1cd570fdb306
Saproi <stefjims@gmail.com>
2018-04-23 22:33:08 -0600

    Insertar en el árbol

---
+++ src/Pruebas.java
@@ -0,0 +1,14 @@
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		BST arbol = new BST();
+		arbol.insertar(5, "Hola Mundo");
+		arbol.insertar(4, -1234);
+		arbol.insertar(7, null);
+		arbol.insertar(1, 1);
+		arbol.insertar(9, "asdlkj");
+		arbol.insertar(5, 5);
+	}
+
+}
