20658a0df973019f2c4251f7c8c11f8b035410c7
Mariano <marianomm@rocketmail.com>
2018-04-25 15:30:04 -0600

    15.29

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -7,26 +7,24 @@ package BinarySearchTree;
 
 public class BinarySearchTree<T extends Comparable<T>> {
     private TreeNode<T> root;
-//----------------------------------------------------//
-    public BinarySearchTree()
-    {
+
+    //----------------------------------------------------//
+    public BinarySearchTree() {
         this.root = null;
     }
-//----------------------------------------------------//
-    public TreeNode<T> getRoot()
-    {
+
+    //----------------------------------------------------//
+    public TreeNode<T> getRoot() {
         return this.root;
     }
 
 
-//----------------------------------------------------//
-    public boolean contains(T data)
-    {
+    //----------------------------------------------------//
+    public boolean contains(T data) {
         return contains(data, this.root);
     }
 
-    private boolean contains(T element, TreeNode<T> node)
-    {
+    private boolean contains(T element, TreeNode<T> node) {
         if (node == null) {
             return false;
         }
@@ -41,14 +39,13 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         return false;
     }
-//----------------------------------------------------//
-    public TreeNode<T> getElement(T element)
-    {
+
+    //----------------------------------------------------//
+    public TreeNode<T> getElement(T element) {
         return getElement(element, this.root);
     }
 
-    private TreeNode<T> getElement(T element, TreeNode<T> node)
-    {
+    private TreeNode<T> getElement(T element, TreeNode<T> node) {
         if (node == null) {
             return null;
         }
@@ -60,9 +57,9 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         return getElement(element, node.getLeft());
     }
-//----------------------------------------------------//
-    public void insert(T data)
-    {
+
+    //----------------------------------------------------//
+    public void insert(T data) {
         TreeNode<T> nodeAux = new TreeNode(data);
         if (this.root == null) {
             this.root = nodeAux;
@@ -71,38 +68,32 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
-    private TreeNode<T> insert(TreeNode<T> NodeAux, TreeNode<T> aux)
-    {
+    private TreeNode<T> insert(TreeNode<T> NodeAux, TreeNode<T> aux) {
         TreeNode<T> nodeAUX = aux;
-        if (NodeAux.getData().compareTo(aux.getData()) < 0)
-        {
+        if (NodeAux.getData().compareTo(aux.getData()) < 0) {
             if (aux.getLeft() == null) {
                 aux.setLeft(NodeAux);
             } else {
                 aux.setLeft(insert(NodeAux, aux.getLeft()));
             }
-        }
-        else if (NodeAux.getData().compareTo(aux.getData()) > 0)
-        {
+        } else if (NodeAux.getData().compareTo(aux.getData()) > 0) {
             if (aux.getRight() == null) {
                 aux.setRight(NodeAux);
             } else {
                 aux.setRight(insert(NodeAux, aux.getRight()));
             }
-        }
-        else {
+        } else {
             System.out.println("Nodo duplicado");
         }
         return nodeAUX;
     }
-//----------------------------------------------------//
-    public void remove(T element)
-    {
+
+    //----------------------------------------------------//
+    public void remove(T element) {
         this.root = remove(element, this.root);
     }
 
-    private TreeNode<T> remove(T element, TreeNode<T> node)
-    {
+    private TreeNode<T> remove(T element, TreeNode<T> node) {
         if (node == null) {
             return null;
         }
@@ -118,9 +109,9 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return node;
     }
 
-//----------------------------------------------------//
-    public T findMin(){
-        if(this.getRoot()==null)
+    //----------------------------------------------------//
+    public T findMin() {
+        if (this.getRoot() == null)
             return null;
         else
             return findMin(this.root);
@@ -128,48 +119,48 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
 
-    private T findMin(TreeNode node){
-        if(node.getLeft()!=null){
+    private T findMin(TreeNode node) {
+        if (node.getLeft() != null) {
             return findMin(node.getLeft());
-        }else{
+        } else {
             return (T) node.getData();
 
         }
     }
-//----------------------------------------------------//
-    public T findMax(){
-    	if(this.getRoot()==null)
-    		return null;
-    	else
-    		return findMax(this.root);
+
+    //----------------------------------------------------//
+    public T findMax() {
+        if (this.getRoot() == null)
+            return null;
+        else
+            return findMax(this.root);
 
     }
 
 
-    private T findMax(TreeNode node){
-        if(node.getRight()!=null){
+    private T findMax(TreeNode node) {
+        if (node.getRight() != null) {
             return findMax(node.getRight());
-        }else{
+        } else {
             return (T) node.getData();
 
         }
     }
-//----------------------------------------------------//
-    public void print()
-    {
+
+    //----------------------------------------------------//
+    public void print() {
         print(this.root);
     }
 
-    private void print(TreeNode<T> node)
-    {
-        if (node != null)
-        {
+    private void print(TreeNode<T> node) {
+        if (node != null) {
             print(node.getLeft());
             System.out.print(node.getData() + ", ");
             print(node.getRight());
         }
     }
 
+}
 
 
 //---------------------------------------------------//
